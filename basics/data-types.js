fe6b6def0aaa867b82c55561efafb35243dbcd62
thkc <khyatic089@gmail.com>
2025-09-08 13:44:29 +0000

    datatypes

---
+++ basics/data-types.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3) //readability of code
+
+//datatypes
+// numbers => range 2 to power 35
+// string => ""
+// undefined => ex. let state; 
+// null => standalone value
+// boolean => true/false
+// symbol => unique
+
+
+let age=20
+let name ="khyati"
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof age)
+console.log(typeof name)
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,13 @@
-console.log("khyati")
+const accountid = 1234567
+let accountname = "khyatic" // let is same as var i.e stores variable value. This is used to avoid the problem of scope
+var accountpass= "12547" // avoid using var keyword becuase of issue in block scope and functional scope
+accountcity = "shahada"
+
+accountstate= "bihar"
+// accountid= 234
+
+// let accountpass; // this is not allowed in js value must be passed 
+ console.log(accountname)
+
+console.table([accountid,accountcity,accountname,accountpass]) //this is another method to print
+
